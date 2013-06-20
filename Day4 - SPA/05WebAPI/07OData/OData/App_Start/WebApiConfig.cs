5ca72063a90db82092b4c20fc7c5e8023c935e77
StephenWalther <Stephen.Walther@superexpert.com>
2013-06-20 07:11:43 -0700

    Added WebAPI OData EntitySetController sample

--- Day4 - SPA/05WebAPI/07OData/OData/App_Start/WebApiConfig.cs
+++ Day4 - SPA/05WebAPI/07OData/OData/App_Start/WebApiConfig.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using OData.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.OData.Builder;
 
 namespace OData {
     public static class WebApiConfig {
@@ -11,6 +13,20 @@ namespace OData {
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // Query Support won't work with EntitySetController unless you call this method
+            config.EnableQuerySupport();
+
+            // Create the odata model
+            var modelBuilder = new ODataConventionModelBuilder();
+            modelBuilder.EntitySet<Product>("ProductsEntitySet");  // Case Sensitive!!!
+
+            // Expose the odata model
+            var model = modelBuilder.GetEdmModel();
+            config.Routes.MapODataRoute("ODataRoute", "odata", model);
+
+
+
         }
     }
 }
